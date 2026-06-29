7929e9b971ca875c821a9977f8ede2002154224b
Niazy <ae8985145@gmail.com>
2026-06-29 19:52:05 +0300

    fw

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+const cors = require('cors')
+const helmet = require('helmet')
+app.use(helmet())
+app.use(cors())
+
+const root_router = require('./apis/root.js')
+
+app.use(root_router)
+
+const port = 3001
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
