66af397cae0fdb56c0865c432059b03ae7411975
niazy00rot <ae8985145@gmail.com>
2026-08-06 04:56:03 +0300

    d

--- database/init.js
+++ database/init.js
@@ -5,7 +5,7 @@ const { pool } = require('./db');
 async function initDB() {
     try {
         const client = await pool.connect();
-        const sql = await fs.readFile(path.join(__dirname, 'init.sql'), 'utf8');
+        const sql = fs.readFileSync(path.join(__dirname, 'init.sql'), 'utf8');
         await client.query(sql);
         console.log('Database initialized successfully');
     } catch (err) {
