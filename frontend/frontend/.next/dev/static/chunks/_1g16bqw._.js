458042bed84f1171b0b095629da59fe2b2c230d0
kareem <kareemmoh1911@gmail.com>
2026-08-17 14:09:19 +0300

    remove unnecessary folder

--- frontend/frontend/.next/dev/static/chunks/_1g16bqw._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/node_modules_0cftf2s._.js","static/chunks/src_providers_ThemeProvider_tsx_0-gj13w._.js","static/chunks/[root-of-the-server]__0vrk7d4._.css","static/chunks/node_modules_next_18ijsau._.js","static/chunks/node_modules_react-icons_fa6_index_mjs_0_ulevj._.js","static/chunks/node_modules_react-icons_fa_index_mjs_01bzxvj._.js","static/chunks/node_modules_react-icons_lib_11_gx9i._.js","static/chunks/node_modules_next-intl_dist_esm_development_0j1zbqz._.js","static/chunks/src_1-oxl5e._.js","static/chunks/src_components_1cf9qb3._.css","static/chunks/_1mizf9d._.js","static/chunks/src_app_[locale]_(main)_landing_scss_1lky7a6.css"],
-    source: "entry"
-});
