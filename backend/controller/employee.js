09ea285f68b47c29c812643e19d2032d11cc97be
niazy00rot <ae8985145@gmail.com>
2026-08-20 23:10:46 +0300

    dfgdf

--- backend/controller/employee.js
+++ backend/controller/employee.js
@@ -1,22 +1,12 @@
 const router = require('express').Router()
 const {get_id} = require('../config/helper.js')
 const {add_employee,delete_employee,get_employees} = require('../service/employee.js')
-const {is_admin} = require('../service/admin.js')
-const {is_employee} = require('../service/employee.js')
 
-router.get('/employee', async(req,res)=>{
+const {authorize_roles} = require('../middleware/auth.js')
+
+router.get('/employee', authorize_roles(['admin']), async(req,res)=>{
     try{
-        const token = req.cookies?.session;
-        if (!token) {return res.status(401).json({error: "Not authenticated"});
-        }
-        const id = get_id(token)
-        if(!id){
-            return res.status(401).json({error: "Invalid or expired token" });
-        }
-        const admin = is_admin(id)
-        if(!admin){
-            return res.status(403).json({ error: "Admin access required" });
-        }
+        
         const employees = await get_employees();
         return res.status(200).json({ employees });
 
@@ -27,7 +17,7 @@ router.get('/employee', async(req,res)=>{
     }
 })
 
-router.post('/employee', async(req,res)=>{
+router.post('/employee', authorize_roles("admin"), async(req,res)=>{
     const {name, email, password,phone} = req.body
     try{
         const token = req.cookies?.session;
@@ -56,20 +46,9 @@ router.post('/employee', async(req,res)=>{
     }
 })
 
-router.delete('/employee/:id', async(req,res)=>{
-    const {id} = req.params
+router.delete('/employee/:id', authorize_roles("admin"), async(req,res)=>{
     try{
-        const token = req.cookies?.session;
-        if (!token) {return res.status(401).json({error: "Not authenticated"});
-        }
-        const id = get_id(token)
-        if(!id){
-            return res.status(401).json({error: "Invalid or expired token" });
-        }
-        const admin = is_admin(id)
-        if(!admin){
-            return res.status(403).json({ error: "Admin access required" });
-        }
+        const {id} = req.params
         const results = delete_employee(id)
         if (results.err){
             console.error('Error occurred while deleting employee:', results.err)
