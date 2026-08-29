0f4a746cfa5651006ff8df7bbd0e251969d0f672
niazy00rot <ae8985145@gmail.com>
2026-08-29 13:34:49 +0300

    sdfdsf

--- backend/middleware/handler.js
+++ backend/middleware/handler.js
@@ -1,11 +1,11 @@
 
-function error_handler(req,res,next,err){
+function error_handler(err,req,res,next){
     console.error(err)
     return res.status(500).json({error: "Internal server error"});
 }
 
 function async_handler(fn){
-    return(res,req,next)=>{
+    return(req,res,next)=>{
         Promise
             .resolve(fn(req,res,next))
             .catch(next)
